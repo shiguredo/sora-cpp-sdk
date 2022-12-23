78b7ec991e0013a6299fc20a6ef8f33a47d152f5
melpon <shigemasa7watanabe+github@gmail.com>
2022-12-23 12:28:51 +0900

    コード整理

--- test/ios/hello/ViewController.mm
+++ test/ios/hello/ViewController.mm
@@ -14,21 +14,21 @@
 #import <sdk/objc/components/audio/RTCAudioSessionConfiguration.h>
 
 void IosAudioInit(std::function<void(std::string)> on_complete) {
-  auto config = [RTCAudioSessionConfiguration webRTCConfiguration];
-  config.category = AVAudioSessionCategoryPlayAndRecord;
-  [[RTCAudioSession sharedInstance] initializeInput:^(NSError* error) {
-    if (error != nil) {
-      on_complete([error.localizedDescription UTF8String]);
-    } else {
-      on_complete("");
-    }
-  }];
+    auto config = [RTCAudioSessionConfiguration webRTCConfiguration];
+    config.category = AVAudioSessionCategoryPlayAndRecord;
+    [[RTCAudioSession sharedInstance] initializeInput:^(NSError* error) {
+        if (error != nil) {
+            on_complete([error.localizedDescription UTF8String]);
+        } else {
+            on_complete("");
+        }
+    }];
 }
 
 void Run() {
-    rtc::LogMessage::LogToDebug(rtc::LS_INFO);
-    rtc::LogMessage::LogTimestamps();
-    rtc::LogMessage::LogThreads();
+    // rtc::LogMessage::LogToDebug(rtc::LS_INFO);
+    // rtc::LogMessage::LogTimestamps();
+    // rtc::LogMessage::LogThreads();
 
     IosAudioInit([](std::string error) {
         if (!error.empty()) {
@@ -36,15 +36,15 @@ void Run() {
         }
     });
     NSString* path = [[NSBundle mainBundle] resourcePath];
-    NSLog(@"path1=%@", path);
+    // NSLog(@"path1=%@", path);
     NSString* path2 = [path stringByAppendingPathComponent:@"model_coeffs"];
-    NSLog(@"path2=%@", path2);
+    NSLog(@"SORA_LYRA_MODEL_COEFFS_PATH=%@", path2);
     setenv("SORA_LYRA_MODEL_COEFFS_PATH", [path2 UTF8String], 1);
     HelloSoraConfig config;
     config.signaling_urls.push_back("シグナリングURL");
     config.channel_id = "チャンネルID";
     config.role = "sendonly";
-    config.mode = HelloSoraConfig::Mode::Lyra;
+    // config.mode = HelloSoraConfig::Mode::Lyra;
     auto hello = sora::CreateSoraClient<HelloSora>(config);
     hello->Run();
 }
