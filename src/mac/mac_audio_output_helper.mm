74af1d78434ae930bac7f6ea35388b1ad2b59f65
tnoho <teaotya@gmail.com>
2023-09-07 20:37:30 +0900

    WebRTC ライセンス表記を入れておく

--- src/mac/mac_audio_output_helper.mm
+++ src/mac/mac_audio_output_helper.mm
@@ -1,4 +1,14 @@
-#include "sora/mac/mac_audio_output_helper.h"
+/*
+ *  Copyright 2018 The WebRTC Project Authors. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+ 
+ #include "sora/mac/mac_audio_output_helper.h"
 
 // WebRTC
 #include <rtc_base/logging.h>
