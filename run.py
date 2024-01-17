c76330938b39470f18506d47c960c693f056a344
enm10k <enm10k@gmail.com>
2024-01-17 10:28:42 +0900

    clang と CUDA のバージョンを上げる

--- run.py
+++ run.py
@@ -1406,8 +1406,8 @@ def install_deps(platform: Platform, source_dir, build_dir, install_dir, debug,
                 install_vpl_args['cmake_args'].append(f"-DCMAKE_CXX_FLAGS={' '.join(cxxflags)}")
             if platform.target.os == 'ubuntu':
                 cmake_args = []
-                cmake_args.append("-DCMAKE_C_COMPILER=clang-15")
-                cmake_args.append("-DCMAKE_CXX_COMPILER=clang++-15")
+                cmake_args.append("-DCMAKE_C_COMPILER=clang-18")
+                cmake_args.append("-DCMAKE_CXX_COMPILER=clang++-18")
                 path = cmake_path(os.path.join(webrtc_info.libcxx_dir, 'include'))
                 cmake_args.append(f"-DCMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES={path}")
                 flags = [
@@ -1485,8 +1485,8 @@ def install_deps(platform: Platform, source_dir, build_dir, install_dir, debug,
             cmake_args.append(f'-DCMAKE_SYSROOT={sysroot}')
         if platform.target.os == 'ubuntu':
             if platform.target.package_name in ('ubuntu-20.04_x86_64', 'ubuntu-22.04_x86_64'):
-                cmake_args.append("-DCMAKE_C_COMPILER=clang-15")
-                cmake_args.append("-DCMAKE_CXX_COMPILER=clang++-15")
+                cmake_args.append("-DCMAKE_C_COMPILER=clang-18")
+                cmake_args.append("-DCMAKE_CXX_COMPILER=clang++-18")
             else:
                 cmake_args.append(
                     f"-DCMAKE_C_COMPILER={cmake_path(os.path.join(webrtc_info.clang_dir, 'bin', 'clang'))}")
@@ -1643,8 +1643,8 @@ def main():
             cmake_args.append(f'-DCMAKE_SYSTEM_VERSION={WINDOWS_SDK_VERSION}')
         if platform.target.os == 'ubuntu':
             if platform.target.package_name in ('ubuntu-20.04_x86_64', 'ubuntu-22.04_x86_64'):
-                cmake_args.append("-DCMAKE_C_COMPILER=clang-15")
-                cmake_args.append("-DCMAKE_CXX_COMPILER=clang++-15")
+                cmake_args.append("-DCMAKE_C_COMPILER=clang-18")
+                cmake_args.append("-DCMAKE_CXX_COMPILER=clang++-18")
             else:
                 cmake_args.append(
                     f"-DCMAKE_C_COMPILER={cmake_path(os.path.join(webrtc_info.clang_dir, 'bin', 'clang'))}")
@@ -1805,8 +1805,8 @@ def main():
                     cmake_args.append(f'-DCMAKE_SYSROOT={sysroot}')
                 if platform.target.os == 'ubuntu':
                     if platform.target.package_name in ('ubuntu-20.04_x86_64', 'ubuntu-22.04_x86_64'):
-                        cmake_args.append("-DCMAKE_C_COMPILER=clang-15")
-                        cmake_args.append("-DCMAKE_CXX_COMPILER=clang++-15")
+                        cmake_args.append("-DCMAKE_C_COMPILER=clang-18")
+                        cmake_args.append("-DCMAKE_CXX_COMPILER=clang++-18")
                     else:
                         cmake_args.append(
                             f"-DCMAKE_C_COMPILER={cmake_path(os.path.join(webrtc_info.clang_dir, 'bin', 'clang'))}")
