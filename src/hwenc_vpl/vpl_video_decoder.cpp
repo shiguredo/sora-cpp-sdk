6b14d057c9fe30caaa9d9e85a9681a1b3be1193a
enm10k <enm10k@gmail.com>
2024-03-21 15:17:16 +0900

    H.265 のデコーダーが動いた

--- src/hwenc_vpl/vpl_video_decoder.cpp
+++ src/hwenc_vpl/vpl_video_decoder.cpp
@@ -125,16 +125,11 @@ std::unique_ptr<MFXVideoDECODE> VplVideoDecoderImpl::CreateDecoderInternal(
   param.mfx.FrameInfo.Height = (height + 15) / 16 * 16;
 
   param.IOPattern = MFX_IOPATTERN_OUT_SYSTEM_MEMORY;
+  param.mfx.GopRefDist = 1;
+  param.AsyncDepth = 1;
 
   if (codec == MFX_CODEC_HEVC) {
-    param.mfx.GopRefDist = 0;
-    param.AsyncDepth = 0;
     param.mfx.CodecProfile = 1;
-    param.mfx.CodecLevel = 20;
-    param.mfx.IdrInterval = 0;
-  } else {
-    param.mfx.GopRefDist = 1;
-    param.AsyncDepth = 1;
   }
 
   //qmfxExtCodingOption ext_coding_option;
