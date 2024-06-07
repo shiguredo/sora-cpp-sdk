0b75b7e57b4b773f0ebbab5075e3c334f3fdd22a
melpon <shigemasa7watanabe+github@gmail.com>
2024-06-07 19:57:45 +0900

    ビルドログを出してみる

--- buildbase.py
+++ buildbase.py
@@ -690,7 +690,6 @@ def build_and_install_boost(
                 [
                     b2,
                     "install",
-                    "-d+0",
                     f'--prefix={os.path.join(install_dir, "boost")}',
                     "--with-json",
                     "--with-filesystem",
