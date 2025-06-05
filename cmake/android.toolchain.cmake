5a54918f0e71121e92241d6783947e742ab52453
melpon <shigemasa7watanabe+github@gmail.com>
2025-06-05 17:49:46 +0900

    誤字

--- cmake/android.toolchain.cmake
+++ cmake/android.toolchain.cmake
@@ -1,6 +1,6 @@
 # android が提供している android.toolchain.cmake は、
-# （当然ながら）Android NDK に内容している C/C++ コンパイラを利用している。
-# しかし libwebrtc は、Chromium の管理している clang を利用しているので、
+# （当然ながら）Android NDK に内包している C/C++ コンパイラを利用している。
+# しかし libwebrtc は Chromium の管理している clang を利用しているので、
 # それを利用するように上書きする。
 include("${ANDROID_OVERRIDE_TOOLCHAIN_FILE}")
 set(CMAKE_C_COMPILER "${ANDROID_OVERRIDE_C_COMPILER}")
