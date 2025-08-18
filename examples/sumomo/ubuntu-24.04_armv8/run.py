fc84a4860758449737c2f618e2dac0adb2a1bc5c
melpon <shigemasa7watanabe+github@gmail.com>
2025-08-18 21:42:48 +0900

    version 変数を deps に変える

--- examples/sumomo/ubuntu-24.04_armv8/run.py
+++ examples/sumomo/ubuntu-24.04_armv8/run.py
@@ -45,7 +45,7 @@ def install_deps(
     local_sora_cpp_sdk_args: List[str],
 ):
     with cd(BASE_DIR):
-        version = read_version_file("DEPS")
+        deps = read_version_file("DEPS")
 
         # multistrap を使った sysroot の構築
         conf = os.path.join(BASE_DIR, "multistrap", "ubuntu-24.04_armv8.conf")
@@ -62,7 +62,7 @@ def install_deps(
         # WebRTC
         if local_webrtc_build_dir is None:
             install_webrtc_args = {
-                "version": version["WEBRTC_BUILD_VERSION"],
+                "version": deps["WEBRTC_BUILD_VERSION"],
                 "version_file": os.path.join(install_dir, "webrtc.version"),
                 "source_dir": source_dir,
                 "install_dir": install_dir,
@@ -110,8 +110,8 @@ def install_deps(
         # Sora C++ SDK, Boost
         if local_sora_cpp_sdk_dir is None:
             install_sora_and_deps(
-                version["SORA_CPP_SDK_VERSION"],
-                version["BOOST_VERSION"],
+                deps["SORA_CPP_SDK_VERSION"],
+                deps["BOOST_VERSION"],
                 "ubuntu-24.04_armv8",
                 source_dir,
                 install_dir,
@@ -127,7 +127,7 @@ def install_deps(
 
         # CMake
         install_cmake_args = {
-            "version": version["CMAKE_VERSION"],
+            "version": deps["CMAKE_VERSION"],
             "version_file": os.path.join(install_dir, "cmake.version"),
             "source_dir": source_dir,
             "install_dir": install_dir,
@@ -140,7 +140,7 @@ def install_deps(
         # SDL3
         sysroot = os.path.join(install_dir, "rootfs")
         install_sdl3_args = {
-            "version": version["SDL3_VERSION"],
+            "version": deps["SDL3_VERSION"],
             "version_file": os.path.join(install_dir, "sdl3.version"),
             "source_dir": source_dir,
             "build_dir": build_dir,
@@ -166,7 +166,7 @@ def install_deps(
 
         # CLI11
         install_cli11_args = {
-            "version": version["CLI11_VERSION"],
+            "version": deps["CLI11_VERSION"],
             "version_file": os.path.join(install_dir, "cli11.version"),
             "install_dir": install_dir,
         }
