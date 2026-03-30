3cae1b762f24e2fd391d85c974c25d9d9d21b4a7
torikizi <51085972+torikizi@users.noreply.github.com>
2026-03-30 10:52:08 +0900

    LS_ERROR ではなく LS_WARNING にする

--- src/hwenc_amf/amf_video_encoder.cpp
+++ src/hwenc_amf/amf_video_encoder.cpp
@@ -541,7 +541,7 @@ AMF_RESULT AMFVideoEncoderImpl::ProcessBuffer(amf::AMFBufferPtr buffer,
   webrtc::EncodedImageCallback::Result result =
       callback_->OnEncodedImage(encoded_image_, &codec_specific);
   if (result.error != webrtc::EncodedImageCallback::Result::OK) {
-    RTC_LOG(LS_ERROR) << __FUNCTION__
+    RTC_LOG(LS_WARNING) << __FUNCTION__
                       << " OnEncodedImage failed error:" << result.error;
   }
   bitrate_adjuster_.Update(size);
