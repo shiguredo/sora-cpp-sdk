9a14084c886f8a065565604c403fc79dbb88c07e
melpon <shigemasa7watanabe+github@gmail.com>
2026-05-21 15:47:19 +0900

    VPL デコーダの MaxLength 二重加算を修正

--- src/hwenc_vpl/vpl_video_decoder.cpp
+++ src/hwenc_vpl/vpl_video_decoder.cpp
@@ -232,7 +232,7 @@ int32_t VplVideoDecoderImpl::Decode(const webrtc::EncodedImage& input_image,
 
   if (bitstream_.MaxLength < bitstream_.DataLength + input_image.size()) {
     bitstream_buffer_.resize(bitstream_.DataLength + input_image.size());
-    bitstream_.MaxLength = bitstream_.DataLength + bitstream_buffer_.size();
+    bitstream_.MaxLength = bitstream_buffer_.size();
     bitstream_.Data = bitstream_buffer_.data();
   }
   //printf("size=%zu\n", input_image.size());
