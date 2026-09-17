ecd747a39dbb7d979526de286dd9519896ae1746
melpon <shigemasa7watanabe+github@gmail.com>
2026-09-18 06:08:29 +0900

    0059 V4L2 エンコーダでフレームのペアリングがズレるのを修正する
    - VIDIOC_QBUF より先にコールバックを登録し、フレームの結果が失われないようにする
    - capture バッファのタイムスタンプと照合し、ズレを検知したら登録を破棄して復帰する
    - 投入に失敗したバッファを再利用可能に戻し、枯渇しないようにする

--- include/sora/hwenc_v4l2/v4l2_runner.h
+++ include/sora/hwenc_v4l2/v4l2_runner.h
@@ -62,14 +62,50 @@ class V4L2Runner {
       int dst_memory,
       std::function<void()> on_change_resolution = nullptr);
 
+  // 出力された capture バッファ (v4l2_buffer) と、そのバッファをデバイスに戻す
+  // ための on_next を受け取るコールバック
+  //
+  // このコールバックでは、必要な処理が終わったら最後に必ず on_next を呼ぶこと。
   typedef std::function<void(v4l2_buffer*, std::function<void()>)>
       OnCompleteCallback;
 
+  // 入力フレームをデバイスに投入し、それに対応する capture バッファが出力された
+  // ときに呼ぶ on_complete を登録する
+  //
+  // v4l2_buf の timestamp にはデキュー時の健全性の確認に利用するため、
+  // 必ず適切な値を設定しておくこと
+  //
+  // 失敗した場合は WEBRTC_VIDEO_CODEC_ERROR を返す。このとき v4l2_buf の出力
+  // バッファは再利用可能に戻してあり、on_complete は呼ばれない (そのフレームの
+  // 結果は得られないので、必要なら呼び出し側で再投入すること)
   int Enqueue(v4l2_buffer* v4l2_buf, OnCompleteCallback on_complete);
 
+  // 利用可能な出力バッファのインデックスを取り出す
+  //
+  // 取得したインデックスは Enqueue に渡すこと。利用可能なバッファが無い場合は
+  // std::nullopt を返す
   std::optional<int> PopAvailableBufferIndex();
 
  private:
+  // v4l2_buf のタイムスタンプを us に変換する
+  static int64_t BufferTimestampUs(const v4l2_buffer* v4l2_buf);
+
+  // 利用中の出力バッファを利用可能に戻す
+  //
+  // 出力バッファを利用可能にする経路はこの関数に統一する
+  void ReturnAvailableOutputBuffer(int index);
+
+  // Enqueue で登録した 1 フレーム分の情報
+  struct Registration {
+    int64_t timestamp_us;
+    OnCompleteCallback on_complete;
+  };
+
+  // デバイスが出力した capture バッファをデバイスに戻す
+  //
+  // on_complete に渡す on_next と、対応する登録が無かった場合の再エンキューで使う
+  static void EnqueueCaptureBuffer(int fd, v4l2_buffer* v4l2_buf);
+
   void PollProcess();
 
  private:
@@ -81,7 +117,7 @@ class V4L2Runner {
   std::function<void()> on_change_resolution_;
 
   ConcurrentQueue<int> output_buffers_available_;
-  ConcurrentQueue<OnCompleteCallback> on_completes_;
+  ConcurrentQueue<Registration> on_completes_;
   std::atomic<bool> abort_poll_;
   webrtc::PlatformThread thread_;
 };
--- src/hwenc_v4l2/v4l2_runner.cpp
+++ src/hwenc_v4l2/v4l2_runner.cpp
@@ -7,6 +7,7 @@
 // WebRTC
 #include <modules/video_coding/include/video_error_codes.h>
 #include <rtc_base/logging.h>
+#include <rtc_base/time_utils.h>
 
 namespace sora {
 
@@ -44,24 +45,55 @@ std::shared_ptr<V4L2Runner> V4L2Runner::Create(
       [p = p.get()]() { p->PollProcess(); }, "PollThread",
       webrtc::ThreadAttributes().SetPriority(webrtc::ThreadPriority::kHigh));
   for (int i = 0; i < src_count; i++) {
-    p->output_buffers_available_.push(i);
+    p->ReturnAvailableOutputBuffer(i);
   }
   return p;
 }
 
 int V4L2Runner::Enqueue(v4l2_buffer* v4l2_buf, OnCompleteCallback on_complete) {
+  // VIDIOC_QBUF より先に登録する。デバイスは投入された入力をすぐ処理できるため、
+  // 登録が後だと対応する capture バッファを先に取り出してしまうことがある
+  on_completes_.push(Registration{BufferTimestampUs(v4l2_buf), on_complete});
   if (ioctl(fd_, VIDIOC_QBUF, v4l2_buf) < 0) {
     RTC_LOG(LS_ERROR) << __func__ << "  Failed to queue output buffer: error="
                       << strerror(errno);
+    on_completes_.pop();
+    // VIDIOC_QBUF に失敗したバッファはデバイスに渡っていないので、DQBUF では
+    // 返却されない。そのままでは利用可能なバッファが減り続けるため、ここで
+    // 再利用可能に戻す
+    ReturnAvailableOutputBuffer(v4l2_buf->index);
     return WEBRTC_VIDEO_CODEC_ERROR;
   }
-  on_completes_.push(on_complete);
 
   return WEBRTC_VIDEO_CODEC_OK;
 }
 
 std::optional<int> V4L2Runner::PopAvailableBufferIndex() {
   return output_buffers_available_.pop();
+}
+
+void V4L2Runner::ReturnAvailableOutputBuffer(int index) {
+  output_buffers_available_.push(index);
+}
+
+int64_t V4L2Runner::BufferTimestampUs(const v4l2_buffer* v4l2_buf) {
+  return (int64_t)v4l2_buf->timestamp.tv_sec * webrtc::kNumMicrosecsPerSec +
+         v4l2_buf->timestamp.tv_usec;
+}
+
+// デバイスが出力した capture バッファをデバイスに戻す。
+//
+// on_complete に渡す on_next と、対応する登録が無かった場合の再エンキューで使う。
+void V4L2Runner::EnqueueCaptureBuffer(int fd, v4l2_buffer* v4l2_buf) {
+  // 呼び出し元の v4l2_buf が持つ planes は呼び出し元のスタックを指しているため、
+  // この関数のスタックに用意した planes を指し直してから QBUF する
+  v4l2_buffer v4l2_capture_buf = *v4l2_buf;
+  v4l2_plane planes[VIDEO_MAX_PLANES] = {};
+  v4l2_capture_buf.m.planes = planes;
+  if (ioctl(fd, VIDIOC_QBUF, &v4l2_capture_buf) < 0) {
+    RTC_LOG(LS_ERROR) << "Failed to enqueue capture buffer: error="
+                      << strerror(errno);
+  }
 }
 
 void V4L2Runner::PollProcess() {
@@ -108,7 +140,7 @@ void V4L2Runner::PollProcess() {
                           << "] Failed to dequeue output buffer: error="
                           << strerror(errno);
       } else {
-        output_buffers_available_.push(v4l2_buf.index);
+        ReturnAvailableOutputBuffer(v4l2_buf.index);
       }
 
       v4l2_buf = {};
@@ -127,22 +159,46 @@ void V4L2Runner::PollProcess() {
         if (abort_poll_) {
           break;
         }
-        std::optional<OnCompleteCallback> on_complete = on_completes_.pop();
-        if (!on_complete) {
-          RTC_LOG(LS_ERROR)
-              << "[POLL][" << name_ << "] on_completes_ is empty.";
-          if (ioctl(fd_, VIDIOC_QBUF, &v4l2_buf) < 0) {
-            RTC_LOG(LS_ERROR) << "Failed to enqueue capture buffer: error="
-                              << strerror(errno);
+
+        // タイムスタンプが一致する登録を探す。通常は on_completes_ の先頭の要素と一致するが、
+        //
+        // - QBUF していないのに DQBUF が発生される
+        // - QBUF した要素がスキップされる
+        //
+        // というケースが発生した場合の復帰用となる。
+        // （仕様上そのようなことは起きるはずはないが念のため）。
+        // 一致しない登録は破棄する。
+        int64_t capture_timestamp_us = BufferTimestampUs(&v4l2_buf);
+        std::optional<Registration> registration = on_completes_.pop();
+        int discard_count = 0;
+        int64_t discarded_timestamp_us = 0;
+        while (registration &&
+               registration->timestamp_us != capture_timestamp_us) {
+          if (discard_count == 0) {
+            discarded_timestamp_us = registration->timestamp_us;
           }
+          discard_count++;
+          registration = on_completes_.pop();
+        }
+        if (discard_count > 0) {
+          RTC_LOG(LS_WARNING)
+              << "[POLL][" << name_ << "] Pairing is out of sync. Discarded "
+              << discard_count
+              << " frame(s): capture timestamp_us=" << capture_timestamp_us
+              << " discarded timestamp_us=" << discarded_timestamp_us;
+        }
+
+        int fd = fd_;
+        if (!registration) {
+          // 一致する登録が無いので、この capture バッファをデバイスに戻す
+          RTC_LOG(LS_WARNING)
+              << "[POLL][" << name_ << "] on_completes_ is empty."
+              << " Requeue the capture buffer:"
+              << " capture timestamp_us=" << capture_timestamp_us;
+          EnqueueCaptureBuffer(fd, &v4l2_buf);
         } else {
-          (*on_complete)(&v4l2_buf, [fd = fd_, v4l2_buf]() mutable {
-            v4l2_plane planes[VIDEO_MAX_PLANES] = {};
-            v4l2_buf.m.planes = planes;
-            if (ioctl(fd, VIDIOC_QBUF, &v4l2_buf) < 0) {
-              RTC_LOG(LS_ERROR) << "Failed to enqueue capture buffer: error="
-                                << strerror(errno);
-            }
+          registration->on_complete(&v4l2_buf, [fd, v4l2_buf]() mutable {
+            EnqueueCaptureBuffer(fd, &v4l2_buf);
           });
         }
       }
